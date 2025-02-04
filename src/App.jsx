55076f4393ff46cd6b6544826c2e44be4acbfa23
nehalskdev <nikesk1999@gmail.com>
2025-02-04 14:22:19 +0530

    added accordian compoenent

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,16 @@ export default function App() {
   );
 }
 
+function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      {data.map((el, i) => (
+        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
+      ))}
+    </div>
+  );
+}
+
 
 
 
